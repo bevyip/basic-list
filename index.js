5c78f39b5cd2f72b1b415cad921e9b4efb5eb7a0
hyip8000 <hyip@purdue.edu>
2017-06-06 16:38:39 -0400

    Managed to change colors of top of list on click!

--- index.js
+++ index.js
@@ -7,8 +7,23 @@ function handleSubmit(e){
     const details = document.querySelector('#details') 
     
     var newItem = document.createElement("li")
+    newItem.classList.add('notclicked')
     var textNode = document.createTextNode(name)
     newItem.appendChild(textNode);
+    newItem.addEventListener("click", function(){
+        myFunction()
+    });
+
+    function myFunction(){
+        if (newItem.classList.contains('notclicked')){
+            document.getElementById('details').children[0].classList.add('clicked')
+            document.getElementById('details').children[0].classList.remove('notclicked')
+        }else{
+            document.getElementById('details').children[0].classList.add('notclicked')
+            document.getElementById('details').children[0].classList.remove('clicked')
+        }
+    }
+
     details.insertBefore(newItem, details.childNodes[0])
     
 }
