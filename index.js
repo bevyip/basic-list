43f7142aab9c2b013f7fac5d7db8ed43ed5bdc80
hyip8000 <hyip@purdue.edu>
2017-06-06 14:25:12 -0400

    Accidental edit

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const personForm = document.querySelector('#personForm')
  
+function renderListItem(name){
+        const li = document.createElement('li')
+        li.innerHTML = `Name: ${name}`
+        
+        return li
+    }
+
+function renderList(name){
+        const list = document.createElement('ul')
+        
+        //loop over ['name, 'favColor', 'age']
+        Object.keys(name).map(function(fieldName){
+            const li = renderListItem(name)
+            list.appendChild(li)
+        })
+        
+        return list 
+    }
 
 function handleSubmit(e){
     e.preventDefault()
     const f = e.target
     const name = f.name.value
-    const details = document.querySelector('#details')
-        
+    const details = document.querySelector('#details') 
     
+    details.appendChild(renderList(name))
 }
 personForm.addEventListener("submit",handleSubmit)
